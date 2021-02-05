002c2633cf64ce16f9c40b601add345cbefe4475
nathaliadv <nathaliadantasv@gmail.com>
2021-02-05 16:41:09 -0300

    commit inicial

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser');
-const router = require ("./src/routes"); 
+const router = require ("./routes"); 
+
+
+app.use('/', router);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 require('dotenv').config();
 const port = process.env.PORT || 8081; //definindo a porta
---
+++ routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+
+const routes = new Router;
+
+routes.get('/', (req, res) => {});
+router.post('/', (req, res) => {});
+
+module.exports = routes;
