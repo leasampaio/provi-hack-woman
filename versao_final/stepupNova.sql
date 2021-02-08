b3c0294b5153ca7795967177e9c63d172767de44
Nathalia Dantas Viana <nathaliadantasv@gmail.com>
2021-02-07 22:28:46 -0300

    Update stepupNova.sql

--- versao_final/stepupNova.sql
+++ versao_final/stepupNova.sql
@@ -7,9 +7,9 @@ create table usuarias(
 	email varchar(30),
 	senha varchar(20),
 	area_interesse varchar(50),
-	hab_tecnicas text, --habilidades tecnicas multiplos valores
-	outras_hab_tecnicas text, --outras habilidades tecnicas multiplos valores
-	hab_socioemocionais text, --habilidades socioemocionais multiplos valores
+	hab_tecnicas text, 
+	outras_hab_tecnicas text, 
+	hab_socioemocionais text, 
 	sobre_voce text,
 	portfolio varchar(50),
 	linkedin varchar(50),
@@ -20,9 +20,9 @@ create table usuarias(
 	descricao_empresa text,
 	mudanca_cidade varchar(10),
 	escola varchar(50),
-    justificativa_indicacao text,
-    idiomas varchar(50),
-    cidade varchar(50)
+    	justificativa_indicacao text,
+    	idiomas varchar(50),
+    	cidade varchar(50)
 );
 
 
@@ -31,8 +31,8 @@ create table empresas(
 	nome varchar(50),
 	email varchar(30),
 	senha varchar(20),
-	hab_tecnicas text, --não sei mais se precisa
-	hab_socioemocionais text, --não sei mais se precisa
+	hab_tecnicas text, 
+	hab_socioemocionais text, 
 	video_apresentacao varchar(50),
 	ramo_atuacao varchar(30)
 );
